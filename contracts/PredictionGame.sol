edd5ac058669d3dc6af4bcc651faf5036ee8a23c
Sharath <sharathkrml@gmail.com>
2022-05-25 19:09:02 +0530

    prediction logic finished

--- contracts/PredictionGame.sol
+++ contracts/PredictionGame.sol
@@ -2,23 +2,53 @@
 
 pragma solidity ^0.8.0;
 
-contract PredictionGame{
-    struct Prediction{
+import "hardhat/console.sol";
+
+contract PredictionGame {
+    struct Prediction {
         uint value;
         uint time;
+        uint difference;
         address from;
     }
-    event NewPrediction(
-        uint value,
-        uint time,
-        address from
-    );
-    Prediction[10] public predictions;
-    uint public count;
+    event NewPrediction(uint value, uint time, address from);
+    event Result(Prediction[]);
+    Prediction[] public predictions;
+
     function predict(uint _value) external {
-        require(count<10,"maximum predictions completed");
-        predictions[count]=Prediction(_value,block.timestamp,msg.sender);
+        require(predictions.length < 10, "maximum predictions completed");
+        predictions.push(Prediction(_value, block.timestamp, 0, msg.sender));
         emit NewPrediction(_value, block.timestamp, msg.sender);
-        count++;
+    }
+
+    function _getDifference(uint _actualValue) internal {
+        for (uint i = 0; i < predictions.length; i++) {
+            predictions[i].difference = predictions[i].value >= _actualValue
+                ? predictions[i].value - _actualValue
+                : _actualValue - predictions[i].value;
+        }
+    }
+
+    function getResult(uint _actualValue) external {
+        // emit Result and reset
+        Prediction memory temp;
+        _getDifference(_actualValue);
+        for (uint i = 0; i < predictions.length; i++) {
+            for (uint j = 0; j < predictions.length - i - 1; j++) {
+                // if current amt > next amt
+                if (
+                    predictions[j].difference > predictions[j + 1].difference ||
+                    (predictions[j].difference ==
+                        predictions[j + 1].difference &&
+                        predictions[j].time > predictions[j + 1].time)
+                ) {
+                    temp = predictions[j];
+                    predictions[j] = predictions[j + 1];
+                    predictions[j + 1] = temp;
+                }
+            }
+        }
+        emit Result(predictions);
+        delete predictions;
     }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -11,16 +11,56 @@ describe("PredictionGame", () => {
     await GameContract.deployed();
     [owner, ...accounts] = await ethers.getSigners();
   })
-  describe('check prediction', async () => {
-    it("new prediction", async () => {
+  // describe('check prediction', async () => {
+  //   it("new prediction", async () => {
+  //     for (let i = 0; i < 10; i++) {
+  //       let a = await GameContract.connect(accounts[i]).predict(i+1);
+  //       console.log(await a.wait());
+  //     }
+  //     let count = await GameContract.count();
+  //     for (let i = 0; i < count; i++) {
+  //       console.log(await GameContract.predictions(i));
+  //     }
+  //   })
+  // })
+
+  describe('check getResult', async () => {
+    it("getResult", async () => {
       for (let i = 0; i < 10; i++) {
-        let a = await GameContract.connect(accounts[i]).predict(i+1);
-        console.log(await a.wait());
+        let a = await GameContract.connect(accounts[i]).predict(i + 1);
       }
-      let count = await GameContract.count();
-      for (let i = 0; i < count; i++) {
-        console.log(await GameContract.predictions(i));
+      // let count = await GameContract.count();
+      // for (let i = 0; i < count; i++) {
+      //   console.log(await GameContract.predictions(i));
+      // }
+      let res = await GameContract.getResult(5);
+      let a = await res.wait()
+      for (let b in a.events[0].args[0]) {
+        console.log(a.events[0].args[0][b]);
       }
+      // let count = await GameContract.count();
+      // for (let i = 0; i < count; i++) {
+      //   console.log(await GameContract.predictions(i));
+      // }
+    })
+    it("Check some difficult", async () => {
+      let arr = [3,4,5,4,3,5,4,3,2,1]
+      for (let i = 0; i < 10; i++) {
+        let a = await GameContract.connect(accounts[i]).predict(arr[i]);
+      }
+      // let count = await GameContract.count();
+      // for (let i = 0; i < count; i++) {
+      //   console.log(await GameContract.predictions(i));
+      // }
+      let res = await GameContract.getResult(5);
+      let a = await res.wait()
+      for (let b in a.events[0].args[0]) {
+        console.log(a.events[0].args[0][b]);
+      }
+      // let count = await GameContract.count();
+      // for (let i = 0; i < count; i++) {
+      //   console.log(await GameContract.predictions(i));
+      // }
     })
   })
 });
